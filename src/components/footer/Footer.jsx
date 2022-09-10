97ed2b255d9802120cd48e24adc5c7806949c0a1
fateme <abedifateme1378@gmail.com>
2022-09-10 17:54:31 +0430

    Footer added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+function Footer(props) {
+  return <div className={styles.footer}>&copy;2022 All Right Reserved.</div>
+}
+
+export default Footer
